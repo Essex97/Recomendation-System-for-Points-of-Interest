2730d7e697d35eef11c210f5fcd6266a9d157263
fkourou <kourou_rip@hotmail.com>
2018-03-17 22:35:56 +0200

    Fixed worker, after establishing connection to send a string back to the master.

--- src/main/java/distributed/Worker.java
+++ src/main/java/distributed/Worker.java
@@ -1,11 +1,14 @@
 package distributed;
 
-import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.io.*;
 import java.net.*;
 
 public class Worker {
+
+    ObjectOutputStream out;
+
     public static void main(String args[]) {
         new Worker().openServer();
     }
@@ -21,6 +24,10 @@ public class Worker {
             connection = providerSocket.accept();
 
             System.out.println("New connection..");
+
+            out = new ObjectOutputStream(connection.getOutputStream());
+            out.writeObject("teststring");
+            out.close();
 
 
         } catch (IOException ioException) {
