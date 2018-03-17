007ccdbb9cf992449aab4a375d7db6c29f982c29
marprok <mariosprokopakis@gmail.com>
2018-03-17 21:18:34 +0200

    Created the WorkerManager class to manage the worker connections.

--- src/main/java/distributed/Master.java
+++ src/main/java/distributed/Master.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 
 public class Master
 {
-    private HashMap<String, Socket> workers;
+    private HashMap<String, WorkerManager> workers;
 
     private void wakeUpWorkers(String path)
     {
@@ -32,14 +32,16 @@ public class Master
             {
                 String[] tokens = lines.get(i).split(" ");
                 int port = Integer.parseInt(tokens[2]);
-                workers.put(tokens[0], new Socket(tokens[1], port));
+                Socket connection = new Socket(tokens[1], port);
+                // Send the hello message to the worker.
+                workers.put(tokens[0], new WorkerManager(connection, tokens[0]));
             }
         } catch (IOException ioe)
         {
             String[] tokens = lines.get(i).split(" ");
             System.out.println("Failed To connect to worker " + tokens[0] + " with IP: " + tokens[1]);
             ioe.printStackTrace();
-        }catch (NumberFormatException nfe)
+        } catch (NumberFormatException nfe)
         {
             nfe.printStackTrace();
         }
---
+++ src/main/java/distributed/WorkerManager.java
@@ -0,0 +1,95 @@
+package distributed;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class WorkerManager
+{
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public boolean isOk()
+    {
+        return isOk;
+    }
+
+    private String name;
+    private Socket connection;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+    private boolean isOk;
+
+    public WorkerManager(Socket con, String name)
+    {
+        this.isOk = true;
+        this.connection = con;
+        this.name = name;
+        this.in = null;
+        this.out = null;
+        try
+        {
+            in = new ObjectInputStream(connection.getInputStream());
+            out = new ObjectOutputStream(connection.getOutputStream());
+        } catch (IOException ioe)
+        {
+            isOk = false;
+            ioe.printStackTrace();
+        }
+    }
+
+    public void sendData(Object obj)
+    {
+        try
+        {
+            out.writeObject(obj);
+        } catch (IOException ioe)
+        {
+            isOk = false;
+            ioe.printStackTrace();
+        }
+    }
+
+    public Object readData()
+    {
+        Object ret = null;
+        try
+        {
+            ret = in.readObject();
+        } catch (IOException ioe)
+        {
+            isOk = false;
+            ioe.printStackTrace();
+        } catch (ClassNotFoundException cnfe)
+        {
+            isOk = false;
+            cnfe.printStackTrace();
+        }
+
+        return ret;
+    }
+
+    public void close()
+    {
+        try
+        {
+            in.close();
+            out.close();
+            connection.close();
+        } catch (IOException ioe)
+        {
+            isOk = false;
+            ioe.printStackTrace();
+        }
+    }
+
+
+}
